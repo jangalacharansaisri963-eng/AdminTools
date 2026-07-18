bed1dfcaf47fc7440e30f00fd439816fc03adfac
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-07-18 20:27:46 +0530

    Refactor command registration to use CommandManager

--- src/main/java/com/yourname/admintools/events/CommandRegistrationHandler.java
+++ src/main/java/com/yourname/admintools/events/CommandRegistrationHandler.java
@@ -1,16 +1,21 @@
-package com.yourname.admintools;  
-  
-import com.yourname.admintools.commands.utility.NearbyCommand;  
-import net.minecraftforge.event.RegisterCommandsEvent;  
-import net.minecraftforge.eventbus.api.SubscribeEvent;  
-import net.minecraftforge.fml.common.Mod;  
-  
-@Mod.EventBusSubscriber(modid = "admintools")  
-public class CommandRegistrationHandler {  
-  
-    @SubscribeEvent  
-    public static void onRegisterCommands(RegisterCommandsEvent event) {  
-        // This passes the game's CommandDispatcher to your NearbyCommand class  
-        NearbyCommand.register(event.getDispatcher());  
-    }  
+package com.yourname.admintools;
+
+import com.yourname.admintools.commands.CommandManager;
+
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = "admintools")
+public class CommandRegistrationHandler {
+
+    @SubscribeEvent
+    public static void onRegisterCommands(RegisterCommandsEvent event) {
+
+        CommandManager.register(
+                event.getDispatcher()
+        );
+
+    }
+
 }
