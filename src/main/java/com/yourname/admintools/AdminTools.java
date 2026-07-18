bc33e4f489b22223320343c71c61e80583ecb67d
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-07-18 20:38:25 +0530

    Fix command registration in AdminTools class

--- src/main/java/com/yourname/admintools/AdminTools.java
+++ src/main/java/com/yourname/admintools/AdminTools.java
@@ -8,38 +8,29 @@ import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-
 @Mod("admintools")
 public class AdminTools {
 
-
     public static final String MOD_ID = "admintools";
-
 
     public AdminTools() {
 
-
         BanManager.load();
-
 
         MinecraftForge.EVENT_BUS.register(this);
 
-
     }
-
 
     @SubscribeEvent
     public void registerCommands(
             RegisterCommandsEvent event
     ) {
 
-
         CommandManager.register(
-                event.getDispatcher()
+                event.getDispatcher(),
+                event.getBuildContext()
         );
 
-
     }
-
 
 }
