8b3421beddcd492e283c3e5844f4a95a2649686f
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-07-19 05:50:22 +0530

    Add IItemStackExtension interface for enchantments

---
+++ src/main/java/com/yourname/admintools/api/IItemStackExtension.java
@@ -0,0 +1,7 @@
+package com.yourname.admintools.api;
+
+import net.minecraft.world.item.enchantment.Enchantment;
+
+public interface IItemStackExtension {
+    void admintools$addEnchantment(Enchantment enchantment, int level);
+}
