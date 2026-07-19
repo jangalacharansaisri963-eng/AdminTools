d3b686ca8d88166d8e51e72949dda70f7f773753
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-07-19 06:26:16 +0530

    Add applyGodPower method to IItemStackExtension

--- src/main/java/com/yourname/admintools/api/IItemStackExtension.java
+++ src/main/java/com/yourname/admintools/api/IItemStackExtension.java
@@ -3,5 +3,9 @@ package com.yourname.admintools.api;
 import net.minecraft.world.item.enchantment.Enchantment;
 
 public interface IItemStackExtension {
+    // Keeps your original enchantment method
     void admintools$addEnchantment(Enchantment enchantment, int level);
+    
+    // Adds the new power method
+    void admintools$applyGodPower(int level);
 }
