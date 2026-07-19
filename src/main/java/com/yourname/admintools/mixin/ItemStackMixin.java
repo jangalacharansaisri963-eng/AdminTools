999b7e8167ed90719e1c2559d373e27836d9ebf5
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-07-19 06:27:15 +0530

    Implement enchantment and god power methods in ItemStackMixin
    Added methods to apply enchantments and god power to ItemStack.

--- src/main/java/com/yourname/admintools/mixin/ItemStackMixin.java
+++ src/main/java/com/yourname/admintools/mixin/ItemStackMixin.java
@@ -1,6 +1,9 @@
 package com.yourname.admintools.mixin;
 
 import com.yourname.admintools.api.IItemStackExtension;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
@@ -8,23 +11,36 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
 import java.util.Map;
+import java.util.UUID;
 
 @Mixin(ItemStack.class)
 public class ItemStackMixin implements IItemStackExtension {
 
     @Unique
+    private static final UUID GOD_DAMAGE_UUID = UUID.fromString("CB3F55D3-645C-4F38-A497-9C13A33DB5CF");
+
+    @Unique
     @Override
     public void admintools$addEnchantment(Enchantment enchantment, int level) {
-        // Cast 'this' to ItemStack to interact with it as a proper object
+        ItemStack self = (ItemStack) (Object) this;
+        Map<Enchantment, Integer> enchantments = EnchantmentHelper.getEnchantments(self);
+        enchantments.put(enchantment, level);
+        EnchantmentHelper.setEnchantments(enchantments, self);
+    }
+
+    @Unique
+    @Override
+    public void admintools$applyGodPower(int level) {
         ItemStack self = (ItemStack) (Object) this;
         
-        // Get the current enchantments on the item
-        Map<Enchantment, Integer> enchantments = EnchantmentHelper.getEnchantments(self);
+        // Remove existing attribute to prevent stacking
+        self.removeAttributeModifier(Attributes.ATTACK_DAMAGE, GOD_DAMAGE_UUID);
         
-        // Add the new enchantment (this will overwrite existing levels of the same enchantment)
-        enchantments.put(enchantment, level);
-        
-        // Save the updated map back to the item stack
-        EnchantmentHelper.setEnchantments(enchantments, self);
+        // Add new damage attribute
+        self.addAttributeModifier(
+            Attributes.ATTACK_DAMAGE, 
+            new AttributeModifier(GOD_DAMAGE_UUID, "GodPower", (double) level, AttributeModifier.Operation.ADDITION), 
+            EquipmentSlot.MAINHAND
+        );
     }
 }
