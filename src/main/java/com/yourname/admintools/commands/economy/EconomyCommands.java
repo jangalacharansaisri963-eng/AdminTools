0d603e6924c80e8040ef72ab736ac5588e9c8d3f
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-07-20 19:23:49 +0530

    Refactor EconomyCommands to organize command registration

--- src/main/java/com/yourname/admintools/commands/economy/EconomyCommands.java
+++ src/main/java/com/yourname/admintools/commands/economy/EconomyCommands.java
@@ -1,51 +1,63 @@
 package com.yourname.admintools.commands.economy;
 
-
 import com.mojang.brigadier.CommandDispatcher;
 
 import net.minecraft.commands.CommandSourceStack;
 
-
-
 public class EconomyCommands {
-
 
     public static void register(
             CommandDispatcher<CommandSourceStack> dispatcher
-    ){
+    ) {
 
-
+        // Economy
         BalanceCommand.register(dispatcher);
+        BankCommand.register(dispatcher);
 
-        ShopCommand.register(dispatcher);
-
-        SellCommand.register(dispatcher);
-
-        BuyCommand.register(dispatcher);
-
-        MyShopCommand.register(dispatcher);
-
-        SellCancelCommand.register(dispatcher);
-
-        AvailableWorksCommand.register(dispatcher);
-
-        AcceptWorkCommand.register(dispatcher);
-
-
-        AddMoneyCommand.register(dispatcher);
-
-        SetMoneyCommand.register(dispatcher);
-
-        RemoveMoneyCommand.register(dispatcher);
+        DepositCommand.register(dispatcher);
+        WithdrawCommand.register(dispatcher);
 
         SendMoneyCommand.register(dispatcher);
 
+        AddMoneyCommand.register(dispatcher);
+        RemoveMoneyCommand.register(dispatcher);
+        SetMoneyCommand.register(dispatcher);
 
-        SetupWorkCommand.register(dispatcher);
+        RewardCommand.register(dispatcher);
+        FineCommand.register(dispatcher);
 
-        CancelWorkCommand.register(dispatcher);
         EcoCommand.register(dispatcher);
 
+        // Shop
+        ShopCommand.register(dispatcher);
+        BuyCommand.register(dispatcher);
+        SellCommand.register(dispatcher);
+
+        MyShopCommand.register(dispatcher);
+        SellCancelCommand.register(dispatcher);
+
+        // Work
+        AvailableWorksCommand.register(dispatcher);
+        AcceptWorkCommand.register(dispatcher);
+
+        SetupWorkCommand.register(dispatcher);
+        CancelWorkCommand.register(dispatcher);
+        WorkDoneCommand.register(dispatcher);
+
+        // Bank
+        SetBankNameCommand.register(dispatcher);
+
+        // Tax
+        TaxCommand.register(dispatcher);
+        PayTaxCommand.register(dispatcher);
+
+        // Lottery
+        LotteryCommand.register(dispatcher);
+        LotteryBuyCommand.register(dispatcher);
+        LotteryAddCommand.register(dispatcher);
+
+        // Salary
+        SalaryCommand.register(dispatcher);
 
     }
 
